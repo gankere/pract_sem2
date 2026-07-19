22d5be08eeb2cc7a7775174d36352028082e4d5f
Anna Vladimirskaya <annav7822@gmail.com>
2026-07-19 11:50:13 +0700

    Удалена папка build из репозитория

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PodcastUI_FILE C:/Users/gankere/TPU_Workspace/sem4/pract/build/PodcastUI.exe)
-set(__QT_DEPLOY_TARGET_PodcastUI_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PodcastUI_RUNTIME_DLLS C:/msys64/mingw64/bin/Qt6Widgets.dll;C:/msys64/mingw64/bin/Qt6Multimedia.dll;C:/msys64/mingw64/bin/Qt6Gui.dll;C:/msys64/mingw64/bin/Qt6Network.dll;C:/msys64/mingw64/bin/Qt6Concurrent.dll;C:/msys64/mingw64/bin/Qt6Core.dll)
